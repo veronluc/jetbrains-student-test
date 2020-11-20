f3f71084cb9a3a10b5050f1a4fb42249fe13e0c3
Luc Véron <veron.luc@gmail.com>
2020-11-20 23:16:53 +0100

    [front] add editor layout to project

---
+++ src/view/EditorLayoutController.java
@@ -0,0 +1,14 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class EditorLayoutController {
+
+    @FXML
+    private void run() {
+        System.out.println("lol");
+    }
+
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,62 @@
+package view;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Jetbrains students test");
+
+        initRootLayout();
+
+        showEditorLayout();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the editor layout inside the root layout.
+     */
+    public void showEditorLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("EditorLayout.fxml"));
+            AnchorPane editorLayout = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(editorLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
